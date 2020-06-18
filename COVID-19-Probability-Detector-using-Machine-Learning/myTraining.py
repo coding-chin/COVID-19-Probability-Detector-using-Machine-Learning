38b37fcc74fe0911505bafb1fce0d6667e5ea4f6
Chinmaya Naik <51273653+coding-chin@users.noreply.github.com>
2020-06-19 01:49:57 +0530

    Add files via upload

---
+++ COVID-19-Probability-Detector-using-Machine-Learning/myTraining.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+def data_split(data, ratio):
+    np.random.seed(42)
+    shuffled = np.random.permutation(len(data))
+    test_set_size = int(len(data)*ratio)
+    test_indices = shuffled[:test_set_size]
+    train_indices = shuffled[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+if __name__ == "__main__":
+    df = pd.read_csv('data.csv')
+    train, test = data_split(df, 0.2)
+
+    X_train = train[['Fever', 'BodyPain', 'Age', 'RunningNose', 'DifficultBreath']].to_numpy()
+    X_test = test[['Fever', 'BodyPain', 'Age', 'RunningNose', 'DifficultBreath']].to_numpy()
+
+    Y_train = train[['InfectionProbablity']].to_numpy().reshape(2400,)
+    Y_test = test[['InfectionProbablity']].to_numpy().reshape(599,)
+
+    clf = LogisticRegression()
+    clf.fit(X_train, Y_train)
+
+    # open a file, where you ant to store the data
+    file = open('model.pkl', 'wb')
+    
+    # dump information to that file
+    pickle.dump(clf, file)
+    file.close()
